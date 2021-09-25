1a630debe9b35efa3ac9c7d299d576ddc06a57b3
coderjake91 <jacob.b.frazer@gmail.com>
2021-09-25 08:26:55 -0500

    created connection to election database using mysql2 and created first query route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 //import the express module and initialize the PORT settings
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+//import mysql2 module
+const mysql = require('mysql2');
+const { brotliDecompress } = require('zlib');
 
 //server instance
 const app = express();
@@ -8,6 +11,22 @@ const app = express();
 //require express server middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'',
+        password:'',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+//query the database using the query method on the instantiated 'db' object
+db.query(`select * from candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //default response for any other request (Not Found), this 'catchall' route needs to be the last defined route!
